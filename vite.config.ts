9fdd515ca36085e066ee9199a6cec9189adfb6ab
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-11-04 21:02:05 +0300

    Populate env on build during cicd

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 3000,
       host: "0.0.0.0",
+      allowedHosts: ["app3.test.me"],
     },
     plugins: [react()],
     define: {
