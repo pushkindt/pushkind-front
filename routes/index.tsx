9dd07bccd0d0db3fce1025dad6ebb9a38ef442da
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-11-14 16:18:13 +0300

    Fix App remounting on route changes

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,11 +5,7 @@ import App from "../App";
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/categories/:categoryId" element={<App />} />
-      <Route path="/tags/:tagId" element={<App />} />
-      <Route path="/products/:productId" element={<App />} />
-      <Route path="*" element={<App />} />
+      <Route path="/*" element={<App />} />
     </Routes>
   );
 };
