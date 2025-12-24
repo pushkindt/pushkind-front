d8e3af0334fea3d0d347026f8c4f4b27d78e8719
Viacheslav Matrizaev <matrizaev@gmail.com>
2025-12-24 15:41:03 +0300

    Made list layout more compact

--- views/HomeView.tsx
+++ views/HomeView.tsx
@@ -80,7 +80,7 @@ const HomeView: React.FC<HomeViewProps> = ({
         className={
           productLayout === "grid"
             ? "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6"
-            : "flex flex-col gap-6"
+            : "flex flex-col gap-3"
         }
       >
         {products.map((product) => (
